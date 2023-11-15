9700268c7b0a4cf5ea19e9ab657da8ea00c870c2
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2023-11-16 02:34:52 +0530

    Update MyProfile.php

--- VBucks-ECS/MyProfile.php
+++ VBucks-ECS/MyProfile.php
@@ -1,53 +1,57 @@
- <!--Structuring a mega project-->
- <!--get method used for less secured-->
- <!DOCTYPE html>
+
+<!DOCTYPE html>
  <html lang="en">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
+    
     <style>
-        *{
-            
+        * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
             text-decoration: none;
-            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            font-family: sans-serif;
             font-size: large;
         }
-        body b{
-            min-height: 80vh;
+
+        body b {
+            min-height: 50vh; /* Adjusted min-height */
             justify-content: center;
             align-items: center;
-            color:  #153b72;
+            color: #153b72;
             background-color: rgb(219 234 254);
-            width: 105%;
+            width: 120%; /* Adjusted width */
             padding: 10%;
             padding-right: 45px;
             border: #153b72;
-            
         }
-        form{
-            min-height: 80vh;
+
+        form {
+            min-height: 60vh; /* Adjusted min-height */
             justify-content: center;
             align-items: center;
             display: grid;
-            color:  #153b72;
-           
+            color: #153b72;
+            max-width: 800px; /* Added max-width for responsiveness */
+            margin: auto; /* Center the form horizontally */
         }
-        h1{
+
+        h1 {
             align-items: center;
             justify-content: center;
             display: flex;
-            font-size:xx-large;
+            font-size: xx-large;
         }
+
         iframe.footer {
             width: 100%;
-            height: 180px; /* Adjust the height of the iframe as needed */
-            border: none; /* Remove the border to avoid extra space */
+            height: 120px; /* Adjusted height */
+            border: none;
             bottom: 0;
         }
+
         @media screen and (max-width: 768px) {
             h2 {
                 font-size: 1.2rem;
@@ -59,51 +63,81 @@
                 font-size: 1rem;
             }
         }
+
+        button {
+            background-color: rgb(30 58 138);
+            color: #fff;
+            border: none;
+            border-radius: 8px;
+            width: 150px;
+            height: 45px;
+            transition: 0.3s;
+            margin: auto;
+            display: block;
+            text-decoration: none; /* Remove underline from the anchor tag inside the button */
+            text-align: center; /* Center text horizontally */
+            line-height: 45px; /* Center text vertically */
+        }
+        button a {
+            color: #fff; /* Set text color to white */
+            text-decoration: none; /* Remove underline */
+        }
+
+        button:hover a {
+            color: #fff; /* Text color on hover */
+        }
+
+
+        button:hover {
+            background-color: #2563eb; /* Darker shade on hover */
+            box-shadow: 0 0 0 5px #2563eb5f;
+        }
+
     </style>
+
     
  <body>
     <main>
-        <section>
-    <iframe src="header.php" title="Header" width="100%" height="100"></iframe>
-        </section>
-    
-      
-    <form action="">
-        <h1> My Profile</h1>
-        <br><br>
-        
-        <b>
-        Name: 
-        <input class="name" type="text" 
-            placeholder="xyz" .required/> 
+        <iframe src="header.php" title="Header" width="100%" height="100"></iframe>
+     
+        <form action="">
+            <h1> My Profile</h1>
+            <br><br>
+            
+            <b>
+            Name: 
+            <input class="name" type="text" 
+                placeholder="xyz" .required/> 
+                <br><br><br>
+            Username: 
+            <input type="text" 
+                placeholder="xyz#14" .required/> 
+                <br>
+                <br>
+            
+            <br>
+            DOB:
+            <input type="text" 
+                placeholder="15-December-2005" .required/> 
+            <br><br>
+            <br>
+            Phone No.: 
+            <input type="tel" 
+                placeholder="1234567890" />
             <br><br><br>
-        Username: 
-        <input type="text" 
-            placeholder="xyz#14" .required/> 
-            <br>
-            <br>
-        
-        <br>
-        DOB:
-        <input type="text" 
-            placeholder="15-December-2005" .required/> 
-        <br><br>
-        <br>
-        Phone No.: 
-        <input type="tel" 
-            placeholder="1234567890" />
-        <br><br><br>
-        E-mail: 
-        <input type="text" 
-            placeholder="xyz@gmail.com" .required/> 
-        <br><br><br>
-        <button>
-            <a href="EditProfile.php">Edit Your Profile</a> 
-            <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+            E-mail: 
+            <input type="text" 
+                placeholder="xyz@gmail.com" .required/> 
+            <br><br><br>
+            <button>
+                <a href="EditProfile.php">Edit Your Profile</a> 
+                <a href="http://" target="_blank" rel="noopener noreferrer"></a>
             </button> 
-        </b>  
-    </form>
-</main>
-<iframe src="footer.php" title="Footer" class="footer"></iframe>
+            </b> 
+        </form>
+        
+        
+    </main>
+    <iframe src="footer.php" title="Footer" class="footer"></iframe>
  </body>
  </html>
